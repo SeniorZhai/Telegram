32ee00fc4d682f282c98c6eb3b2cb2eceef7404e
zhaitao <370985116@qq.com>
2016-12-07 15:46:22 +0800

    run

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,10 +13,12 @@ public class BuildVars {
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static int BUILD_VERSION = 1155;
     public static String BUILD_VERSION_STRING = "4.6";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 51586; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "54cd7f6c5b2fa5092e2eac5e90bcbfae"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "cabc0fbe37344b1da9e491b9120f9ead"; // https://rink.hockeyapp.net/manage/dashboard
+    public static String HOCKEY_APP_HASH_DEBUG = "aa07008c1dc74fb9eb5af61d6057738a"; // https://rink.hockeyapp.net/manage/dashboard
+    public static String GCM_SENDER_ID = "AIzaSyBQguoECW_fF2DU8OL_zSVsA5binSJEK30";
+    public static String SEND_LOGS_EMAIL = "developer.zhaitao@gmail.com";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
